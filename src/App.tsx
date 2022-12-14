edacc1561da62c9f94d865cdae122424d377b03c
mwpark2014 <mwp2010@gmail.com>
2022-12-14 00:23:49 -0800

    Make rows link to spotify Start removing CRA UI

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import {
 } from './services/AuthService';
 import authConfig from './configs/AuthConfig';
 import useAuth from './hooks/useAuth';
-import logo from './logo.svg';
 import './App.css';
 import PlaylistContainer from './components/PlaylistContainer';
 
@@ -22,17 +21,6 @@ function App() {
     <AuthContext.Provider value={authTokens}>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
           <SpotifyLoginLink />
           {authTokens?.accessToken && <PlaylistContainer />}
           {authTokens?.accessToken && <SpotifyLogoutLink />}
--- src/components/PlaylistContainer.tsx
+++ src/components/PlaylistContainer.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { testTracks } from '../tests/PlaylistContainer.test';
 
+type PlaylistReponse = {
+  items: Item[];
+};
+
 type Item = {
   track: Track;
 };
 
 type Track = {
   name: string;
+  uri: string;
 };
 
 function PlaylistContainer() {
-  const trackRows = testTracks.items.map(item => <div>{item.track.name}</div>);
+  const trackRows = (testTracks as PlaylistReponse).items.map(item => (
+    <div>
+      <a href={item.track.uri}>{item.track.name}</a>
+    </div>
+  ));
   return <div>{trackRows}</div>;
 }
 
